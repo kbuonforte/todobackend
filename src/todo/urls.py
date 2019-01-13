ed741f727ea7ac6f4a7143805210de4fe6ba4575
Ken Buonforte <kbuonforte@gmail.com>
2019-01-13 06:41:42 -0500

    Finished module 5

---
+++ src/todo/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url, include
+from todo import views
+from rest_framework.routers import DefaultRouter
+
+# create a router and register our viewsets with it
+router = DefaultRouter(trailing_slash=False)
+router.register(r'todos', views.TodoItemViewSet)
+
+urlpatterns = [
+    url(r'^', include(router.urls)),
+]
